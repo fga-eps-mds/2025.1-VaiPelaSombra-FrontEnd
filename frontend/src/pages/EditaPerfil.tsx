a3a97bc6cd636db50a85b5e2a79973b43baab7eb
renann.gomes <renannpop11@gmail.com>
2025-05-10 14:49:35 -0300

    Exemplo de useState e UseEffect

---
+++ frontend/src/pages/EditaPerfil.tsx
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from "react"
+import NavigateButton from "../components/NavigateButton"
+import { useAppContext } from "../context/AppContext"
+
+export default function EditaPerfil() {
+
+  // const { theme, setTheme } = useAppContext()
+  const [preferencias, setPreferencias] = useState<string[]>([])
+  const [openModal, setOpenModal] = useState<boolean>(false)
+
+  useEffect(() => {
+    // Simulando uma chamada de API para buscar preferências
+    async function fetchPreferences() {
+      // Aqui você pode fazer uma chamada real para a API
+      // try {
+      //   const response = await fetch("http://localhost:3000/getPreferences");
+      //   if (!response.ok) {
+      //     throw new Error("Failed to fetch preferences");
+      //   }
+      //   const data = await response.json();
+
+      //   setPreferencias(data.preferences || []);
+      // } catch (error) {
+      //   console.error("Error fetching preferences:", error);
+      //   //throw error;
+      // }
+      
+      const fetchedPreferences = ["ler", "viajar", "programar"]
+      setPreferencias(fetchedPreferences)
+      console.log("Preferências:", fetchedPreferences)
+      console.log("OpenModal:" + openModal)
+    }
+
+    fetchPreferences()
+  }, [openModal])
+  //console.log("Preferencias:", preferencias)
+
+  // const handleButtonClick = () => {
+  //   // shalow copy e deep copy
+  //   const preference = "viajar"
+  //   const newPreferences = [...preferencias, preference]
+  //   setPreferencias(newPreferences)
+  //   console.log("Preferencias:", preferencias)
+  // }
+  //setPreferencias(["asdada","asafaac"])
+
+  function handleChangeModal() {
+    setOpenModal(!openModal) 
+  }
+
+  return (
+    <div>
+      <h1>Edite Perfil</h1>
+      {/* //Img de perfil */}
+      <img src="https://via.placeholder.com/150" alt="Perfil" />
+      <div>
+        
+        <div>
+          Dados do Usuário
+        </div>
+        <div>
+          Privacidade
+        </div>
+        <div>
+          Preferencias  
+          <button onClick={handleChangeModal}>Abrir Modal</button>            
+        </div>
+      </div>
+    </div>
+  )
+}
+  
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import ExamplePage from './pages/Example'
+import EditaPerfil from './pages/EditaPerfil'
 
 function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ function AppRoutes() {
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/example-page" element={<ExamplePage />} />
+        <Route path="/edita-perfil" element={<EditaPerfil />} />
       </Routes>
     </Router>
   )
