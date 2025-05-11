fbb1a5f6de882da042eced1bda83b9c30df94abd
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-05-11 17:47:32 -0300

    add signup page

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import ExamplePage from './pages/Example'
+import Signup from './pages/Signup'
 
 function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ function AppRoutes() {
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/example-page" element={<ExamplePage />} />
+        <Route path='/Signup' element = {<Signup />} />
       </Routes>
     </Router>
   )
