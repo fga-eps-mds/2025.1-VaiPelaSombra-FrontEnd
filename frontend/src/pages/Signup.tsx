0caf7682d5b3decc6ccad2735679d3b806184f6c
João Ramos <jonhluhe273@gmail.com>
2025-05-13 22:47:43 -0300

    Alterei algumas de nomemclatura e alguns pequenos erros de sintaxe, torneio o botão confirmar senha como clicável(subimit),adicionei o campo confirmar senha, foi feita a validação dos campos e adiconei o componente ver senha(eye)

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Separator } from "@/components/ui/separator"
 import { Button } from "@/components/ui/button"
 import {
@@ -10,10 +11,11 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Eye, EyeOff } from "lucide-react";
 
 export default function Signup() {
     return (
-        <div className="flex max-w-screen h-scree">
+        <div className="flex max-w-screen h-screen">
 
             <div className="relative flex justify-center items-center w-1/2 h-screen overflow-hidden">
 
@@ -32,37 +34,111 @@ export default function Signup() {
 }
 
 export function CardWithForm() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmpassword, setConfirmPassword] = useState('');
+    const [verSenha, setVerSenha] = useState(false);
+    const [confirmarverSenha, setConfirmarVerSenha] = useState(false);
+    const [erro, setErro] = useState('');
+
+    const isEmpty = (value: string): boolean => value.trim() === '';
+    const isValidEmail = (email: string): boolean =>
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    const isValidPassword = (password: string): boolean =>
+        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password);
+    const isMatch = (a: string, b: string): boolean => a === b;
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        setErro('');
+
+        if (isEmpty(username)) return setErro("Nome é obrigatório");
+        if (!isValidEmail(email)) return setErro("E-mail inválido");
+        if (!isValidPassword(password)) return setErro("Senha fraca (mín. 8 caracteres, letras e números)");
+        if (!isMatch(password, confirmpassword)) return setErro("As senhas não coincidem");
+
+        setUsername('');
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+    };
+
     return (
         <Card className="w-100">
             <CardHeader>
                 <CardTitle className="text-3xl">Crie uma conta</CardTitle>
-                <CardDescription>Crie a sua conta ja!</CardDescription>
+                <CardDescription>Crie a sua conta já!</CardDescription>
             </CardHeader>
             <CardContent>
-                <form>
+                <form onSubmit={handleSubmit}>
 
                     <div className="grid w-full items-center gap-4">
 
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="name">Nome Completo</Label>
-                            <Input required id="name" placeholder="Digite o seu nome completo" type="text" />
+                            <Input required id="name" placeholder="Digite nome completo" type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)} />
+                            {erro === "nome" && <p className="text-red-500 text-sm mt-1">Preencha o nome completo.</p>}
+
                         </div>
 
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="email">Email</Label>
-                            <Input required id="email" placeholder="fulano@email.com" type="email" />
+                            <Input required id="email" placeholder="Digite email" type="email" 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)} />
+                        {erro === "email" && <p className="text-red-500 text-sm mt-1">Email inválido.</p>}
+
                         </div>
 
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="senha">Senha</Label>
-                            <Input required id="senha" placeholder="Sua senha" type="password" />
+                            <div className="relative">
+                            <Input className="pr-10" required id="senha" placeholder="Digite senha" type={verSenha ? "text" : "password"} 
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} />
+
+                            <button
+                                type="button"
+                                onClick={() => setVerSenha((prev) => !prev)}
+                                className= "absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700">
+                                {verSenha ? <EyeOff size={20} /> : <Eye size={20} />}
+                            </button>
+                            </div>
+                            {erro === "senha" && <p className="text-red-500 text-sm mt-1">Senha deve ter pelo menos 6 caracteres.</p>}
                         </div>
+                        <div className="flex flex-col space-y-1.5">
+
+                <Label htmlFor="confirmarSenha">Confirmar Senha</Label>
+                <div className="relative">
+                    <Input
+                    className="pr-10"
+                    required
+                    id="confirmarSenha"
+                    placeholder="Digite a senha novamente"
+                    type={confirmarverSenha ? "text" : "password"}
+                    value={confirmpassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                    <button
+                    type="submit"
+                    onClick={() => setConfirmarVerSenha((prev) => !prev)}
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                    >
+                    {confirmarverSenha ? <EyeOff size={20} /> : <Eye size={20} />}
+                    </button>
+                </div>
+                {erro === "confirmarsenha" && <p className="text-red-500 text-sm mt-1">As senhas não coincidem.</p>}
+                </div>
+
 
                     </div>
                 </form>
             </CardContent>
             <CardFooter className="flex flex-col w-full gap-4">
-                <Button className="w-full" type="submit" style={{ backgroundColor: '#223A60' }}>Criar uma conta</Button>
+                <Button className="w-full bg-[#223A60] hover:bg-[#2F4A80] text-white transition-colors" onClick={() => alert("Usuário cadastrado com sucesso!")}>Criar uma conta</Button>
 
                 <div className="flex items-center w-full gap-4">
                     <Separator className="flex-1 h-px bg-border" />
