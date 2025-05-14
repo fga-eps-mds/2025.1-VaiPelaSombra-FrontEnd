6c9454623b3cc03c73094b48d3dba81999f1b85d
Kodomoppoi <jcmm.esft@gmail.com>
2025-05-14 19:56:18 -0300

    Criação da página do Plano De Viagens e adição de sua rota no routes.
     Relacionado à issue da documentação: fga-eps-mds/2025.1-VaiPelaSombra-docs#19

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import ExamplePage from './pages/Example'
+import PlanoViagens from './pages/Plano-Viagens'
 import Signup from './pages/Signup'
 
 function AppRoutes() {
@@ -10,6 +11,7 @@ function AppRoutes() {
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/example-page" element={<ExamplePage />} />
+        <Route path="/Plano-Viagens" element={<PlanoViagens />} />
         <Route path='/signup' element = {<Signup />} />
       </Routes>
     </Router>
