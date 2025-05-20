cd45808b05f5e02848905e1f887b6646a425da13
renannOgomes <renannpop11@gmail.com>
2025-05-19 21:13:45 -0300

    Removendo bloco de comentário

--- frontend/src/pages/Plano-Viagens.tsx
+++ frontend/src/pages/Plano-Viagens.tsx
@@ -100,28 +100,6 @@ const travelHistory = [
   },
 ];
 
-/*
-function Home() {
-  const { theme, setTheme } = useAppContext()
-
-  return (
-    <div>
-      <Navbar />
-      <h1>Plano de Viagens</h1>
-      <p>é aqui que se deveria ter o plano, provavelmente...</p>
-      <p>Tema atual: {theme}</p>
-
-      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-        Alternar tema
-      </button>
-
-      <NavigateButton to="/example-page" label="Ir para a página Example" />
-      <NavigateButton to="/Home" label="Ir para Home" />
-    </div>
-  )
-}
-  export default Home
-*/
 const PlanoViagens: React.FC = () => (
   <div className="plano-viagens-bg">
     <Navbar />
