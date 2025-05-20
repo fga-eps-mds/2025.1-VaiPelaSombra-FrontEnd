ab929f9b903c2837c4ee14eaf80ddd69502aae26
João Ramos <jonhluhe273@gmail.com>
2025-05-20 01:27:13 -0300

    alterando alguns parametros de estilizaçao

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
-import imagem3 from '../assets/imagem3.png';
+import imagem4 from '../assets/imagem4.jpg';
 import Umbrella from '../assets/umbrella.svg';
 import EyeToggle from '../components/ui/eye_toggle';
+import estilo from '../assets/estilo.png';
+
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -56,7 +58,7 @@ function Login() {
     //parte JWT q é token web JS
     const data = await response.json();
     localStorage.setItem("token", data.token);
-    navigate("/dashboard");
+    navigate("/home");
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -78,45 +80,56 @@ function Login() {
 
   return (
     <div className="h-screen w-full flex bg-white">
-      <div className="h-full w-1/2 bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${imagem3})` }}></div>
+      <div className="h-full w-1/2 bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${imagem4})` }}></div>
 
-      <div className="h-full w-1/2 bg-white flex justify-center items-center">
+      <div className="h-full w-1/2 bg-white flex justify-center items-center bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${estilo})` }}>
         <div className="h-[70%] w-[55%] bg-white border-2 border-[#E5E5E5] rounded-[10px] shadow-md">
-          <h1 className="text-3xl font-bold text-[#223A60] pt-6 px-7 pb-7">Fazer login</h1>
+          <h1 className="text-3xl font-bold text-black pt-6 px-7 pb-7">Fazer login</h1>
 
           <form onSubmit={handleSubmit} className="flex flex-col w-full">
             <div className='space-y-7'>
 
               {/* Campo de e-mail */}
               <div className="flex flex-col space-y-1 px-7">
-                <label htmlFor="email" className="text-sm text-[#223A60] font-md">E-mail</label>
+                <label htmlFor="email" className="text-sans text-[#223A60] font-md">E-mail</label>
                 <input
-                  className="w-full border-2 border-[#E5E5E5] rounded-md text-sm pl-2 py-1.5 font-light placeholder-[#9A9A9A]"
+                  className={`w-full border-2 rounded-md text-sm pl-2 py-1.5 font-sm placeholder-[#9A9A9A] ${
+                    emailError ? 'border-red-500' : 'border-[#E5E5E5]'
+                  }`}
                   id="email"
                   type="email"
                   placeholder="Digite o seu e-mail"
                   value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    if (emailError) setEmailError("");
+                  }}
                 />
                 {emailError && (
                   <p className="text-red-500 text-sm mt-1">{emailError}</p>
                 )}
               </div>
 
+
               {/* Campo de senha */}
               <div className="flex flex-col space-y-1 px-7">
                 <div className="flex justify-between items-center">
-                  <label htmlFor="senha" className="text-sm text-[#223A60] font-medium">Senha</label>
+                  <label htmlFor="senha" className="text-sans text-[#223A60] font-md">Senha</label>
                   <a href="#" className="text-sm text-[#223A60] hover:underline font-sans">Esqueceu a senha?</a>
                 </div>
                 <div className="relative w-full">
                   <input
-                    className="w-full border-2 border-[#E5E5E5] rounded-md pl-2 pr-10 py-1.5 text-sm font-light placeholder-[#9A9A9A]"
+                    className={`w-full border-2 rounded-md pl-2 pr-10 py-1.5 text-sm font-sm placeholder-[#9A9A9A] ${
+                      passwordError ? 'border-red-500' : 'border-[#E5E5E5]'
+                    }`}
                     id="senha"
                     type={mostrarSenha ? "text" : "password"}
                     placeholder="Digite a sua senha"
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                      if (passwordError) setPasswordError("");
+                    }}
                   />
                   <div className="absolute right-3 top-[50%] translate-y-[-35%] cursor-pointer">
                     <EyeToggle
@@ -124,11 +137,11 @@ function Login() {
                       toggleVisibility={() => setMostrarSenha(prev => !prev)}
                     />
                   </div>
+                  </div>
+                  {passwordError && (
+                    <p className="text-red-500 text-sm mt-1">{passwordError}</p>
+                  )}
                 </div>
-                {passwordError && (
-                  <p className="text-red-500 text-sm mt-1">{passwordError}</p>
-                )}
-              </div>
 
               {/* Lembrar a senha */}
               <div className="flex items-center justify-between pl-7">
@@ -161,7 +174,12 @@ function Login() {
 
             <div className='w-full pt-4 flex justify-row gap-1 justify-center'>
               <label className='text-sm font-md text-[#283841]'>Não tem uma conta?</label>
-              <a href='#' className='underline text-sm font-md text-[#223A60]'>Cadastre-se</a>
+              <span
+                onClick={() => navigate('/signup')}
+                className="underline text-sm font-md text-[#223A60] cursor-pointer">
+                Cadastre-se
+              </span>
+
             </div>
             {/* Slogan */}
             <div className='w-full pt-3 flex flex-row items-center justify-center gap-1'>
