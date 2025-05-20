3cf5e7f836379c0ce2d66a1e15541fbab45c8a32
Kodomoppoi <jcmm.esft@gmail.com>
2025-05-20 08:55:53 -0300

    -

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,9 +1,8 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/Home'
-import ExamplePage from './pages/Example'
-import PlanoViagens from './pages/Plano-Viagens'
-import Signup from './pages/Signup'
-import PlanoViagens from './pages/Plano-Viagens'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import ExamplePage from './pages/Example';
+import PlanoViagens from './pages/Plano-Viagens';
+import Signup from './pages/Signup';
 
 function AppRoutes() {
   return (
@@ -12,12 +11,11 @@ function AppRoutes() {
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/example-page" element={<ExamplePage />} />
-        <Route path="/Plano-Viagens" element={<PlanoViagens />} />
-        <Route path='/signup' element = {<Signup />} />
-        <Route path="/Plano-Viagens" element={<PlanoViagens />} />
+        <Route path="/planoViagem" element={<PlanoViagens />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default AppRoutes
+export default AppRoutes;
