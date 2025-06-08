fa2689d092b191c9b0d4fe61c312056f8359f7ff
Kodomoppoi <jcmm.esft@gmail.com>
2025-06-08 18:06:50 -0300

    US20-recuperacaoSenha
    Criação do branch
    preparação para fazer a parte de envio de email
    
    criado pasta email em components
    arquivo emailLayout
    emailRoute
    
    US relacionada - "https://github.com/fga-eps-mds/2025.1-VaiPelaSombra-docs/issues/47"

---
+++ frontend/src/components/email/emailRoute.ts
@@ -0,0 +1,4 @@
+//cria de envio de email. "https://react.email" "https://www.youtube.com/watch?v=D4pS4b9-DgA" 6:00 -> END "https://react.email/docs/integrations/resend"
+//serve para mandar em si. ele faz os "gets e posts" com base no backend
+//verifcacoes devem ser feitas na pagina principal, mas um caso de erro nao fará mal
+//deve-se mandar um link de acesso para o id atrelado ao email fornecido, com base no email, gere um link para x id
