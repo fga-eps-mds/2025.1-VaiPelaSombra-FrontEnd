8aa727651b654961d752fe6a629c46784a2f43de
Kodomoppoi <jcmm.esft@gmail.com>
2025-06-09 15:03:42 -0300

    US20-recuperacaoSenha fazendo parte visual da mensagem.
    dependencia de react-email instaladas.
    
    criado um novo ambiente de dev "npm run email" (modificação no package) rodará o exemplo do email a ser enviado para o usuario.
    
    criado a parte visual do email. pode ser visualizado com o comando acima.
    
    US relacionada - "fga-eps-mds/2025.1-VaiPelaSombra-docs#47"

--- frontend/src/components/email/emailLayout.tsx
+++
@@ -1,4 +0,0 @@
-//cria a parte visual do email. "https://react.email" "https://www.youtube.com/watch?v=D4pS4b9-DgA" 00 -> 6:00
-//usar uma layout ja pronto no site do react email e modifica-lo para o contexto
-//esta pagina serve apenas como uma "pagina" visual que o usuario receberá
-//verificaçoes e ter certeza que o email esta correto, deve ser um trabalho para a pagina em si, por mais que um caso de erro nao seja pecado, porem isso sera feito em routes, aqui só é uma area visual.
---
+++ frontend/src/components/emails/emailLayout.tsx
@@ -0,0 +1,111 @@
+//cria a parte visual do email. "https://react.email" "https://www.youtube.com/watch?v=D4pS4b9-DgA" 00 -> 6:00
+//usar uma layout ja pronto no site do react email e modifica-lo para o contexto
+//esta pagina serve apenas como uma "pagina" visual que o usuario receberá
+//verificaçoes e ter certeza que o email esta correto, deve ser um trabalho para a pagina em si, por mais que um caso de erro nao seja pecado, porem isso sera feito em routes, aqui só é uma area visual.
+// o template usado foi o do github. levemente modificado.
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Section,
+  Text,
+} from '@react-email/components';
+
+interface AccessTokenEmailProps {
+  username?: string;//get com database por email, deve ser mandado o email para ca pela pagina principal
+}
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000';
+
+export const AccessTokenEmail = ({
+  username,
+}: AccessTokenEmailProps) => (
+  <Html>
+    <Head />
+    <Body style={main}>
+      <Preview>
+        Clicking the button will give you access to your profile, please change your password as soon as possible
+      </Preview>
+      <Container style={container}>
+
+        <Text style={title}>
+          <strong>@{username}</strong>, a personal link was created.
+        </Text>
+
+        <Section style={section}>
+          <Text style={text}>
+            Hey <strong>{username}</strong>!
+          </Text>
+          <Text style={text}>
+            Clicking the button will give you access to your profile, please change your password as soon as possible
+          </Text>
+
+          <Button style={button} href={`${baseUrl}/profile`}>
+            Click to Enter
+          </Button>
+        </Section>
+
+        <Text style={footer}>
+          Vai pela Sombra
+        </Text>
+      </Container>
+    </Body>
+  </Html>
+);
+
+AccessTokenEmail.PreviewProps = {
+  username: 'alanturing',
+} as AccessTokenEmailProps;
+
+export default AccessTokenEmail;
+
+const main = {
+  backgroundColor: '#ffffff',
+  color: '#24292e',
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji"',
+};
+
+const container = {
+  maxWidth: '480px',
+  margin: '0 auto',
+  padding: '20px 0 48px',
+};
+
+const title = {
+  fontSize: '24px',
+  lineHeight: 1.25,
+};
+
+const section = {
+  padding: '24px',
+  border: 'solid 1px #dedede',
+  borderRadius: '5px',
+  textAlign: 'center' as const,
+};
+
+const text = {
+  margin: '0 0 10px 0',
+  textAlign: 'left' as const,
+};
+
+const button = {
+  fontSize: '14px',
+  backgroundColor: '#28a745',
+  color: '#fff',
+  lineHeight: 1.5,
+  borderRadius: '0.5em',
+  padding: '12px 24px',
+};
+
+const footer = {
+  color: '#6a737d',
+  fontSize: '12px',
+  textAlign: 'center' as const,
+  marginTop: '60px',
+};
