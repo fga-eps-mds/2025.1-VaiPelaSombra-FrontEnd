fa68d247b445efaa74fdab085e6ea0d07112776c
Vitorlustosa <vgui435@gmail.com>
2025-06-08 22:59:14 -0300

    atualizando

--- frontend/src/components/ui/profilecard.tsx
+++ frontend/src/components/ui/profilecard.tsx
@@ -1,31 +1,36 @@
+import React from "react";
+
 type ProfileCardProps = {
   name: string;
   email: string;
   preferencias: string[];
-  avatar: string;
+  avatar?: string;
   onEdit: () => void;
 };
 
-const ProfileCard = ({ name, email, preferencias, avatar, onEdit }: ProfileCardProps) => {
+const ProfileCard: React.FC<ProfileCardProps> = ({ name, email, preferencias, avatar, onEdit }) => {
   return (
-    <div className="flex flex-col items-center space-y-6 px-7 py-8 bg-white rounded-lg shadow-md">
-      <div>
-        <img
-          src={avatar}
-          alt="Avatar do Usuário"
-          className="w-24 h-24 rounded-full object-cover border-2 border-[#223A60]"
-        />
+    <div className="flex flex-col space-y-6 px-7 py-6 bg-white rounded-lg shadow-md">
+      <div className="flex justify-center">
+        {avatar ? (
+          <img
+            src={avatar}
+            alt="Avatar do Usuário"
+            className="w-32 h-32 rounded-full object-cover border-3 border-black mt=5"
+          />
+        ) : (
+          <div className="w-32 h-32 rounded-full bg-gray-200 border-2 border-black" />
+        )}
       </div>
-
-      <div className=" text-center space-y-2">
-        <p className="mt-3"><strong>Nome:</strong> {name}</p>
-        <p className="mt-3"><strong>E-mail:</strong> {email}</p>
-        <p className="mt-3"><strong>Preferências:</strong> {preferencias.join(", ")}</p>
+      <div className="space-y-4 text-sm mt=7">
+        <p><strong>Nome:</strong> {name}</p>
+        <p><strong>E-mail:</strong> {email}</p>
+        <p><strong>Preferências:</strong> {preferencias.join(", ")}</p>
       </div>
       <button
         type="button"
-        className="py-2 px-4 bg-[#223A60] rounded-md text-white font-medium hover:bg-[#2F4A80] transition"
         onClick={onEdit}
+        className="py-2 px-4 bg-[#223A60] text-white rounded-md hover:bg-[#2F4A80] transition self-center"
       >
         Editar Perfil
       </button>
