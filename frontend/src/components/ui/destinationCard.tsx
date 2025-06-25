ee1839bf2354bc6b7624942bfe0154d8ced45029
João Ramos <jonhluhe273@gmail.com>
2025-06-25 17:18:28 -0300

    alteração da requisição get para /home e correção atributo de Destination de name para title que é a nomenclatura que o banco atual utiliza

--- frontend/src/components/ui/destinationCard.tsx
+++ frontend/src/components/ui/destinationCard.tsx
@@ -3,28 +3,28 @@ import { MapPin } from "lucide-react";
 
 interface Destination {
   id: number;
-  name: string;
+  title: string;
   imageUrl: string;
 }
 
-export default function DestinationCard({ name, imageUrl }: Destination) {
+export default function DestinationCard({ title, imageUrl, id}: Destination) {
   const navigate = useNavigate();
 
   return (
     <div
-      onClick={() => navigate(`/inf-destino/${name}`)}
+      onClick={() => navigate(`/inf-destino/${id}`)}
       className="cursor-pointer rounded-2xl shadow-md bg-white overflow-hidden transition transform hover:scale-105 md:hover:shadow-lg md:hover:scale-[1.03]"
     >
       <img
         src={imageUrl}
-        alt={name}
+        alt={title}
         className="w-full h-44"
       />
 
       <div className="p-4 flex items-center justify-center gap-2">
         <MapPin className="text-[#223A60] w-5 h-5 relative top-[-2px]" />
         <span className="text-lg font-medium text-black leading-none">
-          {name}
+          {title}
         </span>
       </div>
     </div>
