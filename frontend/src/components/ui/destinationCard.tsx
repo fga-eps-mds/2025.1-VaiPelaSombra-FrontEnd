f59c9dde82beb158877648e653eb4d74480ca3ac
João Ramos <jonhluhe273@gmail.com>
2025-07-01 22:32:09 -0300

    atualizando o destinationCard para receber a url corretamente do backend e atualização na interface da home e no map para retornar a imagem conforme o back.

--- frontend/src/components/ui/destinationCard.tsx
+++ frontend/src/components/ui/destinationCard.tsx
@@ -1,14 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import { MapPin } from "lucide-react";
+import NoImage from "../../assets/NoImage.png"
 
 interface Destination {
   id: number;
   title: string;
-  imageUrl: string;
+  imageUrl: string | null;
 }
 
-export default function DestinationCard({ title, imageUrl, id}: Destination) {
+export default function DestinationCard({ title, imageUrl, id }: Destination) {
   const navigate = useNavigate();
+  const API_BASE_URL = 'http://localhost:3000';
+  const fullImageUrl = imageUrl
+    ? `${API_BASE_URL}${imageUrl}`
+    : NoImage;
 
   return (
     <div
@@ -16,12 +21,12 @@ export default function DestinationCard({ title, imageUrl, id}: Destination) {
       className="cursor-pointer rounded-2xl shadow-md bg-white overflow-hidden transition transform hover:scale-105 md:hover:shadow-lg md:hover:scale-[1.03]"
     >
       <img
-        src={imageUrl}
-        alt={title}
+        src={fullImageUrl}
+        alt={`Imagem de ${title}`}
         className="w-full h-44"
       />
 
-      <div className="p-4 flex items-center justify-center gap-2">
+      <div className="p-4 flex items-center justify-center gap-2 transform-gpu will-change-transform backface-hidden">
         <MapPin className="text-[#223A60] w-5 h-5 relative top-[-2px]" />
         <span className="text-lg font-medium text-black leading-none">
           {title}
